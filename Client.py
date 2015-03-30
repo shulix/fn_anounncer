a9a41f253779174f6b309592a1f628342b6c1efc
shulix <shulix@gmail.com>
2015-03-30 11:32:53 +0300

    add logging and role dict

--- Client.py
+++ Client.py
@@ -1,7 +1,11 @@
 __author__ = 'shulih'
 import xmlrpclib
 from socket import socket, AF_INET, SOCK_DGRAM
+import sys
 
+if len(sys.argv) < 2:
+   sys.exit('Usage: %s Node_type (e.g. hpi,spm,trmon,trmof,reports)' % sys.argv[0])
+print sys.argv
 PORT = 50000
 MAGIC = "fn_announcer"
 s = socket(AF_INET, SOCK_DGRAM) #create UDP socket
@@ -11,11 +15,19 @@ s.bind(('', PORT))
 while 1:
     data, addr = s.recvfrom(1024) #wait for a packet
     if not len(data):
-        break
-    if data.startswith(MAGIC):
-        print "got service announcement from", data[len(MAGIC):]
-        msg = MAGIC+'alive'
-        s.sendto(msg, (data[len(MAGIC):], 50001))
-        rpcurl='http://'+data[len(MAGIC):]+':8000'
-        server = xmlrpclib.ServerProxy(rpcurl)
-        print 'Ping:', server.pow()
+         break
+    try:
+        if data.startswith(MAGIC):
+            print "got service announcement from", data[len(MAGIC):]
+            msg = MAGIC+sys.argv[1]
+            s.sendto(msg, (data[len(MAGIC):], 50001))
+            rpcurl='http://'+data[len(MAGIC):]+':8000'
+            server = xmlrpclib.ServerProxy(rpcurl)
+            if sys.argv[2] == 'pow':
+                print 'Ping:', server.pow()
+            elif sys.argv[2] == 'mow':
+                print 'Ping:', server.hello('sukar')
+
+
+    except KeyboardInterrupt:
+        print 'Exiting'
--- Server.py
+++ Server.py
@@ -1,11 +1,26 @@
 #!/usr/bin/python
-__author__ = 'shulih'
+
+"""
+
+"""
+__author__ = 'shuli'
+
 import threading
 from time import sleep
 from socket import socket, AF_INET, SOCK_DGRAM, SOL_SOCKET, SO_BROADCAST, gethostbyname, gethostname
-from SimpleXMLRPCServer import SimpleXMLRPCServer
-from SimpleXMLRPCServer import SimpleXMLRPCRequestHandler
+import SimpleXMLRPCServer
+#from SimpleXMLRPCServer import SimpleXMLRPCRequestHandler
 import sys
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+handler = logging.FileHandler('fn_ac.log')
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
 
 announcement = 5
 bc_port = 50000
@@ -14,9 +29,63 @@ rpc_port=8000
 MAGIC = "fn_announcer"
 my_ip= gethostbyname(gethostname()) #get our IP. Be careful if you have multiple network interfaces or IPs
 
+
+roles_dic = dict()
+roles_dic.setdefault('hpi',[])
+roles_dic.setdefault('spm',[])
+roles_dic.setdefault('reports',[])
+roles_dic.setdefault('trmon',[])
+roles_dic.setdefault('trmof',[])
+
+#Create socket
 s = socket(AF_INET, SOCK_DGRAM) #create UDP socket
 s.bind((my_ip, listen_port))
 s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1) #this is a broadcast socket
+
+
+class LoggingSimpleXMLRPCRequestHandler(SimpleXMLRPCServer.SimpleXMLRPCRequestHandler):
+    """
+    Overrides the default SimpleXMLRPCRequestHandler to support logging.
+    Logs client IP and the XML request and response.
+    """
+    def do_POST(self):
+        clientIP, port = self.client_address
+	# Log client IP and Port
+        logger.info('Client IP: %s - Port: %s' % (clientIP, port))
+        try:
+            # get arguments
+            data = self.rfile.read(int(self.headers["content-length"]))
+            # Log client request
+	    logger.debug('Client request: \n%s\n' % data)
+            response = self.server._marshaled_dispatch(
+                    data, getattr(self, '_dispatch', None)
+                )
+	    # Log server response
+            logger.debug('Server response: \n%s\n' % response)
+	except:
+            #Report internal server error (HTTP 500) if the module has an error
+            self.send_response(500)
+            self.end_headers()
+        else:
+            # got a valid XML RPC response
+            self.send_response(200)
+            self.send_header("Content-type", "text/xml")
+            self.send_header("Content-length", str(len(response)))
+            self.end_headers()
+            self.wfile.write(response)
+
+            # shut down the connection
+            self.wfile.flush()
+            self.connection.shutdown(1)
+
+class REMOTEMETHODS:
+    def hello(self, string):
+        return "Hello %s" % string
+
+
+def pow():
+    return "got RPC"
+
 
 def announcer():
     data = MAGIC+my_ip
@@ -24,32 +93,36 @@ def announcer():
     while 1:
         try:
             s.sendto(data, ('<broadcast>', bc_port))
-            print "sent service announcement"
+            logger.info("sent service announcement")
             sleep(announcement)
         except KeyboardInterrupt:
             print 'Exiting'
 
-
-
-def pow():
-    return "got RPC"
-
-def rpcserver():
-    server = SimpleXMLRPCServer((my_ip, rpc_port), logRequests=True)
+def rpc_server():
+    server = SimpleXMLRPCServer.SimpleXMLRPCServer((my_ip, rpc_port), LoggingSimpleXMLRPCRequestHandler)
     server.register_function(pow)
+    object = REMOTEMETHODS()
+    server.register_instance(object)
     try:
-        print 'Use Control-C to exit'
+        logger.debug('Use Control-C to exit')
         server.serve_forever()
     except KeyboardInterrupt:
-        print 'Exiting'
+        logger.debug('Exiting')
+
+def addRole(addr,role):
+    log = addr,'requesting role ',role
+    logger.debug(log)
+    for nodeType in roles_dic.values():
+        if addr not in nodeType:
+            roles_dic[role].append(addr)
 
 threading.Thread(target=announcer).start()
-threading.Thread(target=rpcserver).start()
+threading.Thread(target=rpc_server).start()
 
 while 1:
     message, address = s.recvfrom(2048)
     if message.startswith(MAGIC):
-        print address[0],'is ', message[len(MAGIC):]
+        addRole(address[0],message[len(MAGIC):])
     try:
         sleep(5)
     except (KeyboardInterrupt, SystemExit):
@@ -58,9 +131,6 @@ while 1:
                 try:
                     thread._Thread__stop()
                 except:
-                    print(str(thread.getName()) + ' could not be terminated')
+                    log = (str(thread.getName()) + ' could not be terminated')
+                    logger.debug(log)
         sys.exit()
-
-
-
-
